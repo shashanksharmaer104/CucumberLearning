78838dbe9aa87b1c4678b917be9b288d7d599008
Shashank Sharma <shashanksharma.er104@gmail.com>
2018-06-16 15:54:01 +0530

    First commit

---
+++ Features/Sample.feature
@@ -0,0 +1,5 @@
+Feature: Login feature
+
+Scenario: Verify that user is able to login with valid username and password
+Given A user is on ToolsQA login page
+Then User is able to login successfully
---
+++ src/TesRunners/CucumberRunner.java
@@ -0,0 +1,12 @@
+package TesRunners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Features",glue="TestCode")
+public class CucumberRunner {
+
+}
